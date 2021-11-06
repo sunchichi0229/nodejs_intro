c0a5a8b1a176a0490e48108f781323dd709f3e33
sunchichi0505 <sunchichi0505@gmail.com>
2021-11-07 01:17:11 +0900

    nodejs_intro Update server.js>createwebpage(21.11.07)

--- server.js
+++ server.js
@@ -4,3 +4,11 @@ const app = express();
 const server = app.listen(3000, () => {
     console.log('Start Server : localhost:3000');
 });
+
+app.get('/', function (req, res) {
+    res.send('Node.js始めまして')
+});
+
+app.get('/about', function (req, res) {
+    res.send('Node.jsとは')
+});
