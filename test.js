9f27c18da6206a747a9cb718af2be8a12dbc763c
sunchichi0505 <sunchichi0505@gmail.com>
2021-11-07 00:57:52 +0900

    nodejs_intro Update test.js>callback(21.11.07)

--- test.js
+++ test.js
@@ -10,5 +10,12 @@ const email = {
 
 
 const send = async (option) => {
-    nodemailer.createTransport(email)
+    nodemailer.createTransport(email).sendMail(option, (error, info) => {
+        if(error) {
+            console.log(error);
+        }else {
+            console.log(info);
+            return info.response;
+        }
+    });
 };
