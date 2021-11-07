014b5f59dfa77c054e6d7aac8cba6c44200174b5
sunchichi0505 <sunchichi0505@gmail.com>
2021-11-08 00:08:45 +0900

    nodejs_intro Update moment.js(21.11.08)

---
+++ moment.js
@@ -0,0 +1,6 @@
+const moment = require("moment");
+
+console.log("today", moment().format("YYYY-MM-DD"));
+console.log("today+1", moment().add(1, "day").format("YYYY-MM-DD"));
+console.log("week", moment().add(1, "week").format("YYYY-MM-DD"));
+console.log("month", moment().add(1, "month").format("YYYY-MM-DD"));
